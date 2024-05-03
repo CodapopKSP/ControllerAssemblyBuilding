46d26e964dc40b13e305378c9b18e72fde1142bd
Codapop <george.dikun91@gmail.com>
2024-05-03 22:14:06 +0800

    Center rec config descriptions.

--- script.js
+++ script.js
@@ -165,9 +165,9 @@ recommendedConfigsButton.addEventListener('click', function() {
     '<button id="btn1" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z2400h1b1b4g1a3f3f5f6&color=rgb(0,0,0)&description=JebediahsCommandDescription\'">Jebediah\'s Command</button>' +
     '<button id="btn2" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z2300b3a4g1f3f6f5&color=rgb(10,136,176)&description=ButtonBoxDescription\'">USS Button Box</button>' +
     '<button id="btn3" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z2300b5h1b3f2a5e1&color=rgb(222,78,5)&description=KeyboardsLamentDescription\'">Keyboard\'s Lament</button>' +
-    '<button id="btn1" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z1200f2a5&color=rgb(52,38,125)&description=KeybCompanion1Description\'">Keyboard Companion 1</button>' +
+    '<button id="btn1" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z1200f2a5&color=rgb(52,38,125)&description=KeybCompanion1Description\'">Keyboard Companion</button>' +
     '<button id="btn1" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z1200f2g1&color=rgb(207,199,163)&description=KeybCompanion2Description\'">Keyboard Companion 2</button>' +
-    '<button id="btn1" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z2200a5b3f4f5&color=rgb(202,202,202)&description=ExtCommandSeat1Description\'">External Command Seat 1</button>' +
+    '<button id="btn1" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z2200a5b3f4f5&color=rgb(202,202,202)&description=ExtCommandSeat1Description\'">External Command Seat</button>' +
     '<button id="btn1" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z2200g1b3f2a4&color=rgb(14,98,38)&description=ExtCommandSeat2Description\'">External Command Seat 2</button>' +
     '<button id="btn1" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z2313h1b3b5f1f6g1z2115c1a3&color=rgb(255,255,255)&description=ValentinasCommandDescription\'">Valentina\'s Command</button>' +
     '<button id="btn1" class="btn btn-primary text-center" onclick="window.location.href=\'https://untitledspacecraft.com/?config=z3111a1h3e1z2413g1b1h1h2d1f3a2f5z3116c1b2f6&color=rgb(173,22,27)&description=MissionControlDescription\'">Mission Control</button>',
@@ -1115,12 +1115,15 @@ function loadController(inputData, color, description) {
     recConfigsDescriptionLabel.style.visibility = 'visible';
     recConfigsDescriptionWrapper.style.width = '70vh';
     recConfigsDescriptionWrapper.style.top = '52%';
+    recConfigsDescriptionWrapper.style.left = '19.2%';
+
   }
   if (description==='MissionControlDescription') {
     recConfigsDescriptionLabel.innerHTML = MissionControlDescription;
     recConfigsDescriptionLabel.style.visibility = 'visible';
     recConfigsDescriptionWrapper.style.width = '50vh';
     recConfigsDescriptionWrapper.style.top = '52%';
+    recConfigsDescriptionWrapper.style.left = '28.2%';
   }
 }
 
